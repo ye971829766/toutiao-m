414db99294b82a2c92f92a45ba33fb9c9251354f
yjr <971829766@qq.com>
2022-04-14 19:41:05 +0800

    项目初始化，引入vant

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import router from './router'
 import store from './store'
 // 加载全局样式
 import '@/styles/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+// 注册使用vant组件库
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
